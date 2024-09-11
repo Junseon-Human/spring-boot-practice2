1975afd1a5d61942be00087256e3dfa77a5723e8
junseon <kimkva@naver.com>
2024-09-11 13:50:37 +0900

    bpro02

---
+++ src/main/java/com/keduit/bpro02/dto/SampleDTO.java
@@ -0,0 +1,16 @@
+package com.keduit.bpro02.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class SampleDTO {
+
+    private Long sno;
+    private String first;
+    private String last;
+    private LocalDateTime regTime;
+}
